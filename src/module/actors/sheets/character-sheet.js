462a0aff3e728ce76760c5f175b98139cd8e353e
Weston Wedding <wes@wawco.com>
2023-06-13 15:19:47 -0700

    Fixed: Foundry v11 sheet crash from default item image stopgap (#93)
    * Fixed: Character sheet crash in Foundry v11 caused by stopgap code that doesn't seem necessary anymore.
    
    * Fixed: Smallcraft and Starship character sheet crashes triggered by API changes in Foundry v11.
    
    This code is old workaround code that doesn't appear necessary anymore; default item images seem to work as intended nowadays.
    
    * Missed some comment cleanup.
    
    * Revert "Missed some comment cleanup."
    
    This reverts commit 62161fffbdb998928c6a948fb4e2f4ba4b910026.

--- src/module/actors/sheets/character-sheet.js
+++ src/module/actors/sheets/character-sheet.js
@@ -75,13 +75,7 @@ export class STACharacterSheet extends ActorSheet {
     if (sheetData.system.reputation < 0) {
       sheetData.system.reputation = 0;
     }
-
-    // Checks if items for this actor have default images. Something with Foundry 0.7.9 broke this functionality operating normally.
-    // Stopgap until a better solution can be found.
-    $.each(sheetData.items, (key, item) => {
-      if (!item.img) item.img = game.sta.defaultImage;
-    })
-
+    
     return sheetData;
   }
 
