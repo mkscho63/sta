cb1f4238b06ac2de6bfdb9dbe69bfacde1aa4e6f
CalinStar <paul@projectmanageit.co.uk>
2024-07-27 17:04:50 +0100

    Update roll.js
    - Update to make the roll.js for "range" a little bit neater

--- src/module/roll.js
+++ src/module/roll.js
@@ -334,11 +334,9 @@ export class STARoll {
     const variablePrompt = game.i18n.format('sta.roll.weapon.damage2e');
     const variable = `<div class='dice-formula'> `+variablePrompt.replace('|#|', item.system.damage)+`</div>`;
 	
-	let weaponrange = game.i18n.format('sta.actor.belonging.weapon.melee');
-	if (item.system.range == 'ranged') weaponrange = game.i18n.format('sta.actor.belonging.weapon.ranged');
-	
 	let tags = '';
-    if (item.system.range) tags += '<div class=\'tag\'> ' + weaponrange + '</div>';
+    if (item.system.range == 'melee') tags += '<div class=\'tag\'> '+game.i18n.format('sta.actor.belonging.weapon.melee')+'</div>';
+    if (item.system.range == 'ranged') tags += '<div class=\'tag\'> '+game.i18n.format('sta.actor.belonging.weapon.ranged')+'</div>';
     if (item.system.hands > 0) tags += '<div class=\'tag\'> ' + item.system.hands + ' ' +game.i18n.format('sta.item.genericitem.handed') +'</div>';
     if (item.system.severity > 0) tags += '<div class=\'tag\'> '+game.i18n.format('sta.item.genericitem.severity') + ' ' + item.system.severity +'</div>';
     if (item.system.qualities.stun) tags += '<div class=\'tag\'> '+game.i18n.format('sta.actor.belonging.weapon.stun')+'</div>';    	
@@ -383,13 +381,11 @@ export class STARoll {
 
     const variablePrompt = game.i18n.format('sta.roll.weapon.damage2e');
     const variable = `<div class='dice-formula'> `+variablePrompt.replace('|#|', calculatedDamage)+`</div>`;
-	
-	let weaponrange = game.i18n.format('sta.actor.belonging.weapon.close');
-	if (item.system.range == 'medium') weaponrange = game.i18n.format('sta.actor.belonging.weapon.medium');
-	if (item.system.range == 'long') weaponrange = game.i18n.format('sta.actor.belonging.weapon.long');
-	
+
 	let tags = '';
-    if (item.system.range) tags += '<div class=\'tag\'> ' + weaponrange + '</div>';
+    if (item.system.range == 'close') tags += '<div class=\'tag\'> '+game.i18n.format('sta.actor.belonging.weapon.close')+'</div>';
+    if (item.system.range == 'medium') tags += '<div class=\'tag\'> '+game.i18n.format('sta.actor.belonging.weapon.medium')+'</div>';    	
+	if (item.system.range == 'long') tags += '<div class=\'tag\'> '+game.i18n.format('sta.actor.belonging.weapon.long')+'</div>';
     if (item.system.type == 'energy') tags += '<div class=\'tag\'> '+game.i18n.format('sta.actor.belonging.weapon.energy')+'</div>';    	
 	if (item.system.type == 'torpedo') tags += '<div class=\'tag\'> '+game.i18n.format('sta.actor.belonging.weapon.torpedo')+'</div>';
 	
