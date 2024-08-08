6aa7f16ee50fee7d1cdfd7c29b3a2c667d9a85fe
CalinStar <paul@projectmanageit.co.uk>
2024-08-08 18:29:48 +0100

    - Wildcard for talent names
    - Adds pattern match for talent names instead of absolute terms, so "Resolute" now works with "Resolute (Human)", or "Human Resolute" for example, applied to all actor sheets

--- src/module/actors/sheets/starship-sheet.js
+++ src/module/actors/sheets/starship-sheet.js
@@ -107,7 +107,7 @@ export class STAStarshipSheet extends ActorSheet {
     // With the total value, creates a new div for each and places it under a child called "bar-shields-renderer".
     function shieldsTrackUpdate() {
       shieldsTrackMax = parseInt(html.find('#structure')[0].value) + parseInt(html.find('#security')[0].value) + parseInt(html.find('#shieldmod')[0].value);
-      if (html.find(`[data-talent-name="${localizedValues.advancedshields}"]`).length > 0) {
+      if (html.find(`[data-talent-name*="${localizedValues.advancedshields}"]`).length > 0) {
         shieldsTrackMax += 5;
       }
       // This checks that the max-shields hidden field is equal to the calculated Max Shields value, if not it makes it so.
@@ -130,7 +130,7 @@ export class STAStarshipSheet extends ActorSheet {
     // With the value, creates a new div for each and places it under a child called "bar-power-renderer".
     function powerTrackUpdate() {
       powerTrackMax = parseInt(html.find('#engines')[0].value);
-      if (html.find('[data-talent-name="Secondary Reactors"]').length > 0) {
+      if (html.find('[data-talent-name*="Secondary Reactors"]').length > 0) {
         powerTrackMax += 5;
       }
       // This checks that the max-power hidden field is equal to the calculated Max Power value, if not it makes it so.
--- src/module/actors/sheets/starship-sheet2e.js
+++ src/module/actors/sheets/starship-sheet2e.js
@@ -107,10 +107,10 @@ export class STAStarshipSheet2e extends ActorSheet {
     // With the total value, creates a new div for each and places it under a child called "bar-shields-renderer".
     function shieldsTrackUpdate() {
       shieldsTrackMax = parseInt(html.find('#structure')[0].value) + parseInt(html.find('#security')[0].value) + parseInt(html.find('#scale')[0].value) + parseInt(html.find('#shieldmod')[0].value);
-      if (html.find(`[data-talent-name="${localizedValues.advancedshields}"]`).length > 0) {
+      if (html.find(`[data-talent-name*="${localizedValues.advancedshields}"]`).length > 0) {
         shieldsTrackMax += 5;
       }
-      if (html.find(`[data-talent-name="${localizedValues.polarizedhullplating}"]`).length > 0) {
+      if (html.find(`[data-talent-name*="${localizedValues.polarizedhullplating}"]`).length > 0) {
         shieldsTrackMax = parseInt(html.find('#structure')[0].value) + parseInt(html.find('#shieldmod')[0].value)
       }
       // This checks that the max-shields hidden field is equal to the calculated Max Shields value, if not it makes it so.
@@ -134,7 +134,7 @@ export class STAStarshipSheet2e extends ActorSheet {
     function powerTrackUpdate() {
 //      powerTrackMax = parseInt(html.find('#engines')[0].value);
       powerTrackMax = 0;
-//      if (html.find('[data-talent-name="Secondary Reactors"]').length > 0) {
+//      if (html.find('[data-talent-name*="Secondary Reactors"]').length > 0) {
 //        powerTrackMax += 5;
 //      }
       // This checks that the max-power hidden field is equal to the calculated Max Power value, if not it makes it so.
@@ -157,13 +157,13 @@ export class STAStarshipSheet2e extends ActorSheet {
     // With the value, creates a new div for each and places it under a child called "bar-crew-renderer".
     function crewTrackUpdate() {
       crewTrackMax = parseInt(html.find('#scale')[0].value) + parseInt(html.find('#crwmod')[0].value);
-        if (html.find(`[data-talent-name="${localizedValues.agingrelic}"]`).length > 0) {
+        if (html.find(`[data-talent-name*="${localizedValues.agingrelic}"]`).length > 0) {
         crewTrackMax += 1;
         }
-        if (html.find(`[data-talent-name="${localizedValues.extensiveautomation}"]`).length > 0) {
+        if (html.find(`[data-talent-name*="${localizedValues.extensiveautomation}"]`).length > 0) {
         crewTrackMax = Math.ceil(crewTrackMax/2);
         }
-        if (html.find(`[data-talent-name="${localizedValues.abundantpersonnel}"]`).length > 0) {
+        if (html.find(`[data-talent-name*="${localizedValues.abundantpersonnel}"]`).length > 0) {
         crewTrackMax += crewTrackMax;
        }  
       // This checks that the max-crew hidden field is equal to the calculated Max Crew Support value, if not it makes it so.
