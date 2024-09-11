6df4c757305d8f1f4a8a03fc98cd10e70621114f
CalinStar <64332855+tokeidlom@users.noreply.github.com>
2024-09-11 21:58:14 +0100

    Update STARoller.js
    For fun, let's include small craft too!

--- src/module/dice/STARoller.js
+++ src/module/dice/STARoller.js
@@ -206,7 +206,7 @@ new Dialog({
         };
 
         const token = canvas.tokens.controlled[0];
-        if (!token || token.actor.type !== 'starship') {
+        if (!token || (token.actor.type !== 'starship' && token.actor.type !== 'smallcraft')) {
         selectedSystemLabel = "STARoller";
         selectedSystemValue = parseInt(html.find('#systemValue').val());
 
@@ -252,7 +252,7 @@ new Dialog({
 
     const token = canvas.tokens.controlled[0];
 
-if (!token || token.actor.type !== 'starship') {
+if (!token || (token.actor.type !== 'starship' && token.actor.type !== 'smallcraft')) {
     let systemsHtml = `
     <div>
       <input type="number" id="systemValue" name="systemValue" min="0" max="20" value="7">
