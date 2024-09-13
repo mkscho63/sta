0f63b143e236923fc60f77a6a2cde9854d404911
Giles Hutton <ghutton@redhat.com>
2024-09-13 16:53:53 +0100

    Fix indentation

--- gulpfile.js
+++ gulpfile.js
@@ -148,16 +148,16 @@ async function clean() {
 }
 
 function defaultDataPath() {
-    switch (process.platform) {
-        case 'win32':
-            return path.resolve(process.env.localappdata, 'FoundryVTT')
-        case 'linux':
-            return path.resolve(process.env.HOME, '.local', 'share', 'FoundryVTT')
-        case 'darwin':
-            return path.resolve(process.env.HOME, 'Library', 'Application Support', 'FoundryVTT')
-        default:
-            throw Error("No known default for platform ${process.platform}")
-    }
+  switch (process.platform) {
+    case 'win32':
+      return path.resolve(process.env.localappdata, 'FoundryVTT')
+    case 'linux':
+      return path.resolve(process.env.HOME, '.local', 'share', 'FoundryVTT')
+    case 'darwin':
+      return path.resolve(process.env.HOME, 'Library', 'Application Support', 'FoundryVTT')
+    default:
+      throw Error("No known default for platform ${process.platform}")
+  }
 }
 
 // Copy files to test location
