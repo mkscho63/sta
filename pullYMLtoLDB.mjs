7b7fece7ba79798e7a1a0e8890dcb87cbf5fff75
CalinStar <64332855+tokeidlom@users.noreply.github.com>
2025-12-11 13:54:28 +0000

    Tooling for Packs
    - You will need to install foundrycli for this.
    - npm install @foundryvtt/foundryvtt-cli --save-dev

---
+++ pullYMLtoLDB.mjs
@@ -0,0 +1,17 @@
+import { compilePack } from '@foundryvtt/foundryvtt-cli';
+import { promises as fs } from 'fs';
+
+const PACKAGE_ID = process.cwd();
+const yaml = true;
+const folders = true;
+
+const packs = await fs.readdir('./packs');
+for (const pack of packs) {
+  if (pack.startsWith(".")) continue;
+  console.log('Packing ' + pack);
+  await compilePack(
+    `./packs/${pack}`,
+    `./src/assets/packs/${pack}`,
+    { yaml, recursive: folders }
+  );
+}
